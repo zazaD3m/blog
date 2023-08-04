dbed8bdc5bdd70c68038e95d01f01bccdfe30b00
zazaD3m <zaza.d3m@gmail.com>
2023-08-04 15:41:43 +0400

    added responsive design for large screens of  articlecard component

--- frontend/src/components/ArticleCard.jsx
+++ frontend/src/components/ArticleCard.jsx
@@ -8,35 +8,39 @@ const ArticleCard = ({ className }) => {
       className={`rounded-xl overflow-hidden shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] ${className}`}
     >
       <img
-        className="w-full object-cover object-center h-auto"
+        className="w-full object-cover object-center h-auto md:h-52 lg:h-38 xl:h-60"
         src={images.Post1Image}
         alt="title"
       />
       <div className="p-5">
-        <h2 className="font-roboto font-bold text-xl text-dark-soft">
+        <h2 className="font-roboto font-bold text-xl text-dark-soft md:text-2xl lg:text-[28px]">
           Future of Work
         </h2>
-        <p className="text-dark-light mt-3 text-sm">
+        <p className="text-dark-light mt-3 text-sm md:text-lg">
           Lorem ipsum dolor sit amet consectetur adipisicing.
         </p>
         <div className="flex justify-between flex-nowrap items-center mt-6">
-          <div className="flex items-center gap-x-2">
-            <img src={images.Post1ProfileImage} alt="" />
+          <div className="flex items-center gap-x-2 md:gap-x-2.5">
+            <img
+              className="w-9 h-9 md:w-10 md:h-10"
+              src={images.Post1ProfileImage}
+              alt=""
+            />
             <div className="flex flex-col">
-              <h4 className="font-bold italic text-dark-soft text-sm">
+              <h4 className="font-bold italic text-dark-soft text-sm md:text-base">
                 Lorem, ipsum.
               </h4>
               <div className="flex items-center gap-x-2">
                 <span className="bg-[#36B37E] w-fit bg-opacity-20 p-1.5 rounded-full">
                   <BsCheckLg className="w-1.5 h-1.5 text-[#36B37E]" />
                 </span>
-                <span className="italic text-dark-light text-xs ">
+                <span className="italic text-dark-light text-xs md:text-sm">
                   Verified writer
                 </span>
               </div>
             </div>
           </div>
-          <span className="font-bold text-dark-light italic text-sm">
+          <span className="font-bold text-dark-light italic text-sm md:text-base">
             02 May
           </span>
         </div>
--- frontend/src/pages/home/container/Articles.jsx
+++ frontend/src/pages/home/container/Articles.jsx
@@ -3,11 +3,15 @@ import ArticleCard from "../../../components/ArticleCard";
 const Articles = () => {
   return (
     <section className="container mx-auto flex flex-wrap md:gap-x-5 gap-y-5 px-5 py-10">
-      <ArticleCard className="w-full md:w-[calc(50%-20px)]" />
-      <ArticleCard className="w-full md:w-[calc(50%-20px)]" />
-      <ArticleCard className="w-full md:w-[calc(50%-20px)]" />
-      <ArticleCard className="w-full md:w-[calc(50%-20px)]" />
-      <ArticleCard className="w-full md:w-[calc(50%-20px)]" />
+      <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.3%-21px)]" />
+      <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.3%-21px)]" />
+      <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.3%-21px)]" />
+      <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.3%-21px)]" />
+      <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.3%-21px)]" />
+      <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.3%-21px)]" />
+      <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.3%-21px)]" />
+      <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.3%-21px)]" />
+      <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.3%-21px)]" />
     </section>
   );
 };
